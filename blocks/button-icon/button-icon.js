15f1bb583d7a9022f7c8a279e947792f59c075ae
jonatansantosvilt <jonatan.santos@vilt-group.com>
2026-04-30 11:22:36 -0300

    change button icon css to Kebab-Case

--- blocks/button-icon/button-icon.js
+++ blocks/button-icon/button-icon.js
@@ -6,7 +6,7 @@ export default function decorate(block) {
   const position = positionRow?.textContent?.trim() || 'left';
 
   if (link && icon) {
-    icon.classList.add('button-icon__icon', `button-icon__icon--${position}`);
+    icon.classList.add('button-icon-icon', `button-icon-icon--${position}`);
 
     if (position === 'left') {
       link.prepend(icon);
