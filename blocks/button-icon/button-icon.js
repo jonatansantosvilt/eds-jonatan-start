7ea7d0771a60ba68e88130eb6054000ade83a40b
jonatansantosvilt <jonatan.santos@vilt-group.com>
2026-04-30 13:43:34 -0300

    lint

--- blocks/button-icon/button-icon.js
+++ blocks/button-icon/button-icon.js
@@ -1,25 +1,25 @@
 export default function decorate(block) {
   const [linkRow, , , typeRow, iconLeftRow, iconRightRow] = block.children;
 
-  const link = linkRow?.querySelector('a');
+  const link = linkRow?.querySelector("a");
   const type = typeRow?.textContent?.trim().toLowerCase();
 
-  const iconLeft = iconLeftRow?.querySelector('img, svg');
-  const iconRight = iconRightRow?.querySelector('img, svg');
+  const iconLeft = iconLeftRow?.querySelector("img, svg");
+  const iconRight = iconRightRow?.querySelector("img, svg");
 
   if (link) {
-    link.classList.add('button-icon');
+    link.classList.add("button-icon");
     if (type) {
       link.classList.add(type);
     }
 
     if (iconLeft) {
-      iconLeft.classList.add('button-icon-icon', 'button-icon-icon--left');
+      iconLeft.classList.add("button-icon-icon", "button-icon-icon--left");
       link.prepend(iconLeft);
     }
 
     if (iconRight) {
-      iconRight.classList.add('button-icon-icon', 'button-icon-icon--right');
+      iconRight.classList.add("button-icon-icon", "button-icon-icon--right");
       link.append(iconRight);
     }
   }
